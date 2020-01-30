3ed44d15cfb0e7add41e8f9e64a51be2c8d81da4
Ferdows Ohid <ferdows.ohid49@gmail.com>
2020-01-31 01:07:03 +0600

    Updates

---
+++ precache-manifest.a37c75454876f5947cc92f46b1edaba3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25dfc4fbf5030930eb3349c9128f15d",
+    "url": "/ferdows49/monster-roledex.git/index.html"
+  },
+  {
+    "revision": "d3662eaaad8d92c6d39d",
+    "url": "/ferdows49/monster-roledex.git/static/css/main.dd40a8d0.chunk.css"
+  },
+  {
+    "revision": "74ed4ef65b6306ab599d",
+    "url": "/ferdows49/monster-roledex.git/static/js/2.7eb7d594.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ferdows49/monster-roledex.git/static/js/2.7eb7d594.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d3662eaaad8d92c6d39d",
+    "url": "/ferdows49/monster-roledex.git/static/js/main.f68f8570.chunk.js"
+  },
+  {
+    "revision": "901685c4c9b2923157a2",
+    "url": "/ferdows49/monster-roledex.git/static/js/runtime-main.e06ce617.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ferdows49/monster-roledex.git/precache-manifest.a37c75454876f5947cc92f46b1edaba3.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ferdows49/monster-roledex.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
